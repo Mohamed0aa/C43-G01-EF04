5120ad0dc39acb9e2536de1ad2223588ae332a72
Kimo-Store <Kimo Store@DESKTOP-RTPEEP9>
2025-03-13 18:35:51 +0200

    main

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -1,10 +1,41 @@
-﻿namespace Demo
+﻿using Demo.Data;
+using Demo.Data_Additions;
+using Demo.Models;
+namespace Demo
 {
     internal class Program
     {
+        static Program()
+        {
+            using TEST_DbContext tEST_DbContext = new TEST_DbContext();
+            Seeding.Seed(tEST_DbContext);
+
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            using (TEST_DbContext tEST_DbContext = new TEST_DbContext())
+            {
+
+                #region ex1 explicit loading
+                var employee = tEST_DbContext.Employees.FirstOrDefault(E => E.Id == 2);
+
+                tEST_DbContext.Entry(employee).Reference(nameof(Employee.department)).Load();
+                Console.WriteLine($"{employee.Name} => {employee.department.Name}");
+                #endregion
+
+
+                #region eager loading
+
+                #endregion
+
+
+                #region lazy loding
+
+                #endregion
+
+
+
+            }
         }
     }
 }
