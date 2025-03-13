0eb336d7886e120df459267be9dd16d41d357d39
Kimo-Store <Kimo Store@DESKTOP-RTPEEP9>
2025-03-13 18:34:51 +0200

    seeding

---
+++ Demo/Data_Additions/Seeding.cs
@@ -0,0 +1,39 @@
+﻿using Demo.Data;
+using Demo.Models;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Demo.Data_Additions
+{
+    internal static class Seeding
+    {
+        public static void Seed(TEST_DbContext seed)
+        {
+            if (!seed.Departments.Any())//Cheack if Department is Empty or not
+            {
+                var departmentsDate = File.ReadAllText($"{Directory.GetCurrentDirectory()}/departments.json");
+                var departments = JsonSerializer.Deserialize<List<Department>>(departmentsDate);
+
+
+                seed.Departments.AddRange(departments);
+                seed.SaveChanges();
+            }
+
+
+            if (!seed.Employees.Any())//Cheack if Department is Empty or not
+            {
+                var employeesDate = File.ReadAllText($"{Directory.GetCurrentDirectory()}/employees.json");
+                var employees = JsonSerializer.Deserialize<List<Employee>>(employeesDate);
+
+
+                seed.Employees.AddRange(employees);
+                seed.SaveChanges();
+            }
+        }
+    }
+}
