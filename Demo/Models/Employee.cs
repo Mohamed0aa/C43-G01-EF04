3ddb5353a120864675335a90a100470a30592e5d
Kimo-Store <Kimo Store@DESKTOP-RTPEEP9>
2025-03-13 18:33:38 +0200

    models

---
+++ Demo/Models/Employee.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo.Models
+{
+    internal class Employee
+    {
+        [Key]
+        public int Id { get; set; }
+
+        
+        [Required]
+        public string Name { get; set; }
+
+        public double Salary { get; set; }
+
+        public int Age { get; set; }
+
+        [Length(100,200)]
+        public string ? Address { get; set; }
+
+        public int? DepartmentId { get; set; }
+        public Department? department { get; set; }
+        
+        public Department? Manage { get; set; }
+
+    }
+}
